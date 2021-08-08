2e1ee64d61bdf2c5c28fd7b379c05eb9b896ea90
Jan T. Sott <git@idleberg.com>
2021-08-09 00:47:42 +0200

    remove bump version script

--- bump_version.js
+++
@@ -1,9 +0,0 @@
-// if fails use `npm link npm` in caniuse-cli folder
-const npm = require('npm');
-const packageJson = require('./package.json');
-
-const version = packageJson.dependencies['caniuse-db'].substr(1);
-
-npm.load(() => {
-  npm.commands.version([version]);
-});
