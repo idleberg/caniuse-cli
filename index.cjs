f7760dbed8e829784c955b350b53f08d563a7512
Jan T. Sott <git@idleberg.com>
2021-08-09 22:38:34 +0200

    fix fallback

--- index.cjs
+++ index.cjs
@@ -219,7 +219,7 @@ Object.keys(data.data).forEach((key) => {
 });
 
 // find and display result
-const name = process.argv[2]?.toLowerCase() || undefined;
+const name = process.argv[2]?.toLowerCase() || '';
 const res = findResult(name);
 
 if (res !== undefined) {
